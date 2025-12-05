23e3042d1e7562a0b6e9edad442cb64bae4c5ec9
211kamee <141332314+211kamee@users.noreply.github.com>
2025-12-05 17:11:45 +0530

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use actix_web::*;
+use serde::*;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -12,6 +13,9 @@ async fn main() -> std::io::Result<()> {
                 }),
             )
             .service(home)
+            .service(display)
+            .service(displaymsg)
+            .service(query_param)
     })
     .bind(("0.0.0.0", 80))?
     .run()
@@ -21,4 +25,27 @@ async fn main() -> std::io::Result<()> {
 #[get("/home")]
 async fn home() -> impl Responder {
     HttpResponse::Ok().body("/home")
+}
+
+#[get("/display")]
+async fn display() -> impl Responder {
+    HttpResponse::Ok().body("/display")
+}
+
+#[get("/display/{msg}")]
+async fn displaymsg(path: web::Path<String>) -> impl Responder {
+    // let msg = format!("/display/{}", path.into_inner());
+    HttpResponse::Ok().body(path.into_inner())
+}
+
+#[get("/query")]
+async fn query_param(query: web::Query<Info>) -> impl Responder {
+    let msg = format!("name: {},\nage: {}", query.name, query.age);
+    HttpResponse::Ok().body(msg)
+}
+
+#[derive(Deserialize)]
+struct Info {
+    name: String,
+    age: u16,
 }
